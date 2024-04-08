f38952d645980c25c49a76dd2c2ee25acad9c9d1
Younes Oubellal <108194849+mr-youbella@users.noreply.github.com>
2024-04-08 17:21:54 +0000

    Fix tests

--- Tests/Functions/fcc_strstr.c
+++ Tests/Functions/fcc_strstr.c
@@ -11,7 +11,9 @@ int	main(void)
     while (i < 8)
     {
         usleep(200000);
-        if (strcmp(ft_strstr(str[i], to_find[i]), result[i]) == 0)
+        if (ft_strstr(str[i], to_find[i]) == NULL)
+            printf(ansi_red "---------------------\n%d - Fault\nTest: (\"%s\", \"%s\")\nExpected: \"%s\"\nbut got: NULL\n---------------------\n" ansi_default, i + 1, str[i], to_find[i], result[i]);
+        else if (strcmp(ft_strstr(str[i], to_find[i]), result[i]) == 0)
             printf(split_line_passed, i + 1);
         else 
             printf(ansi_red "---------------------\n%d - Fault\nTest: (\"%s\", \"%s\")\nExpected: \"%s\"\nbut got: \"%s\"\n---------------------\n" ansi_default, i + 1, str[i], to_find[i], result[i], ft_strstr(str[i], to_find[i]));
