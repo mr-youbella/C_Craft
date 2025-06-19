9125466d112c95bd5560ae34a506ddaae89bd6d4
Younes Oubellal <108194849+mr-youbella@users.noreply.github.com>
2025-06-19 11:01:57 +0100

    Update install.sh

--- Processing/Scripts/install.sh
+++ Processing/Scripts/install.sh
@@ -5,8 +5,8 @@ if [[ -e ~/C_Craft ]]; then
 	rm -rf ~/C_Craft
 fi
 (cd ~ && git clone https://github.com/mr-youbella/C_Craft > /dev/null 2>&1)
-sudo apt-get install whiptail || sudo yum install newt || sudo dnf install newt || brew install newt || pkg install whiptail
-sudo apt-get install make || sudo yum install make || sudo dnf install make || pkg install make
+sudo apt-get install whiptail || sudo yum install newt || sudo dnf install newt || brew install newt || pkg install whiptail || exit(0)
+sudo apt-get install make || sudo yum install make || sudo dnf install make || pkg install make || exit(0)
 echo >> ~/.bashrc; echo "alias ccraft='bash ~/C_Craft/Processing/Scripts/alias.sh'" >> ~/.bashrc
 echo >> ~/.zshrc; echo "alias ccraft='bash ~/C_Craft/Processing/Scripts/alias.sh'" >> ~/.zshrc
 source ~/.bashrc
